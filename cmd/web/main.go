9aa208a7fcf9458930c3efdaca2070c954b4b513
ekiriano <ekiriano34@gmail.com>
2024-06-24 14:18:57 +0700

    end of chapter

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	// Define a new command-line flag with the name 'addr', a default value of
+	// and some short help text explaining what the flag controls. The value of flag
+	// will be stored in the addr variable at runtime
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Importantly, we use the flag.
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	// Create a file server which serves files out of the "./ui/static" directo
-	// Note that the path given to the http.Dir function is relative to the pro
-	// directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with "/static/". For matching paths, we strip t
-	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
