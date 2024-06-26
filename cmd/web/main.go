a91741cc09587bbe1c725bdee6b61c9d00aa6d5a
ekiriano <ekiriano34@gmail.com>
2024-06-26 16:47:08 +0700

    end of chapter

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,20 +7,12 @@ import (
 	"os"
 )
 
-// Define an application struct to hold the application-wide dependencies for the
-// web application. For now we'll only include fields for the two custom logger
-// we'll add more to it as the build progresses.
-
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 }
 
 func main() {
-
-	// Define a new command-line flag with the name 'addr', a default value of
-	// and some short help text explaining what the flag controls. The value of flag
-	// will be stored in the addr variable at runtime
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
@@ -32,21 +24,15 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	// Importantly, we use the flag.
 
-	// Swap the route declarations to use the application struct's methods as t
-	// handler functions.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(), // Call the new app.routes() method
+	}
 
 	infoLog.Printf("Starting server on %s", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
